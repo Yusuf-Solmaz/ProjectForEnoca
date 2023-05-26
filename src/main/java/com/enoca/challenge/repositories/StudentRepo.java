9bd871e05ee72cd8b3d792aaf0e3cd3fa5435b1e
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-05-26 09:30:19 +0300

    Completing Repositories

--- src/main/java/com/enoca/challenge/repositories/StudentRepo.java
+++ src/main/java/com/enoca/challenge/repositories/StudentRepo.java
@@ -2,7 +2,9 @@ package com.enoca.challenge.repositories;
 
 import com.enoca.challenge.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepo extends JpaRepository<Student,Long> {
 
 }
--- src/main/java/com/enoca/challenge/repositories/UniversityRepo.java
+++ src/main/java/com/enoca/challenge/repositories/UniversityRepo.java
@@ -2,6 +2,8 @@ package com.enoca.challenge.repositories;
 
 import com.enoca.challenge.entities.University;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UniversityRepo extends JpaRepository<University,Long> {
 }
